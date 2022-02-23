8c7df98b27de9b0d2e97dda8e815c7d82373fa7f
photon <zphoton0@gmail.com>
2022-02-23 18:11:53 +0300

    medium

--- hw_3/easy.py
+++ hw_3/easy.py
@@ -10,30 +10,45 @@ class WriteInFileMixin:
 
 class MatrixStrMixin:
     def __str__(self):
-        return '\n'.join('\t'.join(map(str, row)) for row in self.value)
+        return self._create_str(self.value, self._calc_dim())
+
+    def _create_str(self, value, depth):
+        if depth == 1:
+            return '\t'.join(map(str, value))
+        return ('\n' * (depth - 1)).join(self._create_str(subarr, depth - 1) for subarr in value)
+
+    def _calc_dim(self):
+        dim = 0
+        val = self.value
+        try:
+            while len(val):
+                val = val[0]
+                dim += 1
+        except TypeError:
+            return dim
 
 
 class Matrix(WriteInFileMixin, MatrixStrMixin):
     def __init__(self, arr, *, copy=False, skip_check=False):
-        self.rows = len(arr)
-        self.cols = len(arr[0])
-        self.shape = (self.rows, self.cols)
+        self._rows = len(arr)
+        self._cols = len(arr[0])
+        self._shape = (self._rows, self._cols)
         if not skip_check:
-            assert all(len(row) == self.cols for row in arr)
+            assert all(len(row) == self._cols for row in arr)
         self.value = arr if not copy else deepcopy(arr)
 
     def __add__(self, other):
-        assert self.shape == other.shape
+        assert self._shape == other._shape
         return Matrix([[i + j for i, j in zip(row1, row2)] for row1, row2 in zip(self.value, other.value)],
                       skip_check=True)
 
     def __mul__(self, other):
-        assert self.shape == other.shape
+        assert self._shape == other._shape
         return Matrix([[i * j for i, j in zip(row1, row2)] for row1, row2 in zip(self.value, other.value)],
                       skip_check=True)
 
     def __matmul__(self, other):
-        assert self.cols == other.rows
+        assert self._cols == other._rows
         transposed = list(zip(*other.value))
         return Matrix([[sum(i * j for i, j in zip(row, col)) for col in transposed] for row in self.value],
                       skip_check=True)
---
+++ hw_3/medium.py
@@ -0,0 +1,43 @@
+import numpy as np
+import numbers
+
+from easy import MatrixStrMixin, WriteInFileMixin
+from numpy.lib.mixins import NDArrayOperatorsMixin
+
+
+class MyArr(NDArrayOperatorsMixin, MatrixStrMixin, WriteInFileMixin):
+    def __init__(self, value):
+        self.value = np.asarray(value)
+
+    _HANDLED_TYPES = (np.ndarray, numbers.Number)
+
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+        # just copied from https://numpy.org/doc/stable/reference/generated/numpy.lib.mixins.NDArrayOperatorsMixin.html
+        out = kwargs.get('out', ())
+        for x in inputs + out:
+            if not isinstance(x, self._HANDLED_TYPES + (MyArr,)):
+                return NotImplemented
+
+        inputs = tuple(x.value if isinstance(x, MyArr) else x
+                       for x in inputs)
+        if out:
+            kwargs['out'] = tuple(
+                x.value if isinstance(x, MyArr) else x
+                for x in out)
+        result = getattr(ufunc, method)(*inputs, **kwargs)
+
+        if type(result) is tuple:
+            return tuple(type(self)(x) for x in result)
+        elif method == 'at':
+            return None
+        else:
+            return type(self)(result)
+
+
+if __name__ == '__main__':
+    matr_1 = MyArr(np.random.randint(0, 10, (10, 10)))
+    matr_2 = MyArr(np.random.randint(0, 10, (10, 10)))
+    # print(matr_1, matr_2, sep='\n\n')
+    (matr_1 + matr_2).write_in_file('artifacts/medium/matrix+.txt')
+    (matr_1 * matr_2).write_in_file('artifacts/medium/matrix_mul.txt')
+    (matr_1 @ matr_2).write_in_file('artifacts/medium/matrix@.txt')
